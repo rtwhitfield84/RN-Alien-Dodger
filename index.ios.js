78854b8d185e0d1d0c82519ba5b74681ac668300
Rico <richardthomaswhitfield@gmail.com>
2017-04-06 14:15:28 -0500

    added player

--- index.ios.js
+++ index.ios.js
@@ -14,6 +14,16 @@ export default class game extends Component {
     return (
         <Image source={require('./app/img/bg.png')} style={styles.container}>
 
+            <Animated.Image source={require('./app/img/player.png')}
+            style={{
+                height: 100,
+                width: 100,
+                position: 'absolute',
+                zIndex: 1,
+                bottom: 150,
+                resizeMode: 'stretch'
+              }}></Animated.Image>
+
         </Image>
 
     );
