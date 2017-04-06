65db5deb328b94cd38296087e2fcd15137809732
Rico <richardthomaswhitfield@gmail.com>
2017-04-06 14:09:12 -0500

    added bg img

--- index.ios.js
+++ index.ios.js
@@ -3,18 +3,29 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  Animated,
+  Image
 } from 'react-native';
 
 export default class game extends Component {
   render() {
     return (
-        <Text>
-          Hello!
-        </Text>
+        <Image source={require('./app/img/bg.png')} style={styles.container}>
+
+        </Image>
 
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+    resizeMode: 'cover'
+  }
+});
 
 AppRegistry.registerComponent('game', () => game);
